83617c569ec0bc108e7e720296faf55e8ab63592
Danh <vutiendanh.0101@gmail.com>
2023-01-28 17:04:37 +0700

    'cssProfile'

--- src/Pages/User/Profile.tsx
+++ src/Pages/User/Profile.tsx
@@ -10,59 +10,80 @@ type Props = {};
 
 const Profile = (props: Props) => {
   const dispatch: DispatchType = useDispatch();
-  const { userLogin,ModalOpen } = useSelector((state: RootState) => {
+  const { userLogin, ModalOpen } = useSelector((state: RootState) => {
     return state.UserReducer;
   });
 
- 
   return (
     <>
       <div className="container">
         <div className="title-profile" style={{ textAlign: "center" }}>
-          <h1 style={{fontSize:'50px',fontWeight:'bold',marginBottom:'25px'}}>Profile</h1>
+          <h1
+            style={{
+              fontSize: "50px",
+              fontWeight: "bold",
+              marginBottom: "25px",
+            }}
+          >
+            Profile
+          </h1>
         </div>
         <div className="profile-wrap row">
-          <div className="col-6">
-            <img width={400} height={400} style={{borderRadius:'50%',transform:'translateX(38%)'}} src="https://ui-avatars.com/api/?name=Bao%20Toan" alt="..." />
+          <div className="col-lg-6 col-xs-12 col-md-12 col-sm-12">
+            <img
+              width={200}
+              height={200}
+              style={{ borderRadius: "50%", transform: "translateX(38%)" }}
+              src={userLogin.avatar}
+              alt="..."
+            />
           </div>
-          <div className="col-6">
+          <div className="col-lg-6 col-xs-12 col-md-12 col-sm-12">
             <div className="profile-content">
               <div className="form-group">
-                <p className="mb-2" style={{fontSize:'24px'}}>
+                <p className="mb-2" style={{ fontSize: "24px" }}>
                   Id <span className="required-icon text-danger">*</span>
                 </p>
-                <Input size='large'  value={userLogin.id} disabled={true} />
+                <Input size="large" value={userLogin.id} disabled={true} />
               </div>
               <div className="form-group">
-                <p className="my-2" style={{fontSize:'24px'}}>
+                <p className="my-2" style={{ fontSize: "24px" }}>
                   Email <span className="required-icon text-danger">*</span>
                 </p>
-                <Input size='large'  defaultValue={userLogin.email} readOnly />
+                <Input size="large" defaultValue={userLogin.email} readOnly />
               </div>
               <div className="form-group">
-                <p className="my-2" style={{fontSize:'24px'}}>
+                <p className="my-2" style={{ fontSize: "24px" }}>
                   Name <span className="required-icon text-danger">*</span>
                 </p>
-                <Input  size='large' defaultValue={userLogin.name} readOnly/>
+                <Input size="large" defaultValue={userLogin.name} readOnly />
               </div>
               <div className="form-group">
-                <p className="my-2" style={{fontSize:'24px'}}>
-                  Phone number <span className="required-icon text-danger">*</span>
+                <p className="my-2" style={{ fontSize: "24px" }}>
+                  Phone number{" "}
+                  <span className="required-icon text-danger">*</span>
                 </p>
-                <Input  size='large' defaultValue={userLogin.phoneNumber} readOnly/>
+                <Input
+                  size="large"
+                  defaultValue={userLogin.phoneNumber}
+                  readOnly
+                />
               </div>
-             
             </div>
             <div className="group-button d-flex mt-3">
-          
-              <Button type="primary" htmlType="button" size="large" onClick={() => {
-                dispatch(setModalOpen(true))
-              }}>
+              <Button
+                type="primary"
+                htmlType="button"
+                size="large"
+                onClick={() => {
+                  dispatch(setModalOpen(true));
+                }}
+              >
                 Update profile
               </Button>
             </div>
-              {<UpdateProfile/>}
-        
+            {<UpdateProfile />}
+
             {/* <UpdateProfile /> */}
           </div>
         </div>
